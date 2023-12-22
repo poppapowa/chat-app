0655705910b92b53ccc10ab4d966127b45f83a22
Alex Pawelczyk <alex.j.pawelczyk@gmail.com>
2023-12-22 16:39:04 -0500

    Added user typing activity

--- server/index.js
+++ server/index.js
@@ -12,24 +12,42 @@ const PORT = process.env.PORT || 3500;
 const app = express();
 
 // static server for hosting frontend web app on express server
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 const expressServer = app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+    console.log(`listening on port ${PORT}`);
+});
 
 const io = new Server(expressServer, {
     cors: {
         // if web app is not hosted on the express server (i.e. has different domain than express server)
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
-})
+});
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`);
 
+    // upon connection - only to user
+    socket.emit('message', 'Welcome to Chat App!');
+
+    // upon connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`);
+
+    // listening for a message event
     socket.on('message', data => {
         console.log(data);
+        // io.emit sends message to everyone connected to server
         io.emit('message', `${socket.id.substring(0,5)}: ${data}`);
-    })
+    });
+
+    // when user disconnects - to all others
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`)
+    });
+
+    // listen for activity
+    socket.on('activity', (data) => {
+        socket.broadcast.emit('activity', data)
+    });
 })
--- server/public/app.js
+++ server/public/app.js
@@ -1,21 +1,37 @@
-const socket = io('ws://localhost:3500')
+const socket = io('ws://localhost:3500');
+
+const activity = document.querySelector('.activity');
+const msgInput = document.querySelector('input');
 
 function sendMessage(e) {
-    e.preventDefault()
-    const input = document.querySelector('input')
-    if (input.value) {
-        socket.emit('message', input.value)
-        input.value = ""
+    e.preventDefault();
+    if (msgInput.value) {
+        socket.emit('message', msgInput.value);
+        msgInput.value = "";
     }
-    input.focus()
+    msgInput.focus();
 }
 
 document.querySelector('form')
-    .addEventListener('submit', sendMessage)
+    .addEventListener('submit', sendMessage);
 
 // Listen for messages 
 socket.on("message", (data) => {
-    const li = document.createElement('li')
-    li.textContent = data
-    document.querySelector('ul').appendChild(li)
-})
+    activity.textContent = '';
+    const li = document.createElement('li');
+    li.textContent = data;
+    document.querySelector('ul').appendChild(li);
+});
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', socket.id.substring(0, 5))
+});
+
+let activityTimer
+socket.on('activity', (data) => {
+    activity.textContent = `${data} is typing...`;
+    clearTimeout(activityTimer);
+    activityTimer = setTimeout(() => {
+        activity.textContent = '';
+    }, 3000);
+});
